b8a7d9aab1ce7f9231d70e0cd9f5055161fc88da
Wayne Eaker <wayne@zengenuity.com>
2015-07-24 14:04:42 -0400

    Fix db dump/restore scripts to read from yaml files.

--- db_backup.sh
+++ db_backup.sh
@@ -4,6 +4,8 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/config.sh"
 
+MYSQL_PASSWORD=$(get_var config_mysql_password)
+
 mysqldump -u root --password=$MYSQL_PASSWORD --add-drop-database --flush-privileges --routines --all-databases > /var/data/db_backup.sql
 
 if [ -f /var/data/db_backup.sql.gz ]; then
--- db_restore.sh
+++ db_restore.sh
@@ -4,6 +4,8 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/config.sh"
 
+MYSQL_PASSWORD=$(get_var config_mysql_password)
+
 if [ -f /var/data/db_backup.sql.gz ]; then
     gunzip -k /var/data/db_backup.sql
     mysql -u root --password=$MYSQL_PASSWORD < /var/data/db_backup.sql
